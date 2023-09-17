07c1f15d8f4f968d34c1960369114a2c29b2be91
Rezapahlevi3108 <rezapahlv31@gmail.com>
2023-09-17 23:04:10 +0700

    page (landing) : add most viewed posting on landing page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Posting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LandingController extends Controller
 {
     function index() {
-        return view('landing.index');
+        $data = Posting::where('status', 1)->take(8)->orderBy('see','desc')->get();
+        return view('landing.index',compact('data'));
     }
 }
--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -8,7 +8,7 @@
                     <h1 class="text-center font-fredoka font-size-68 font-weight-600 line-height-76 color-primary-2">Kamu itu sangat <br> <span class="font-fredoka color-primary-1">Berharga</span></h1>
                     <p class="text-center font-size-16 font-weight-400 mt-3 d-none d-md-block">“kamu terlalu berharga untuk kalimat <b>“...aku gk berguna...”</b>. Kamu hebat sudah bisa <br> bertahan sampai detik ini, hal menyakitkan apapun yang pernah kamu alami membuat <br> kamu jadi sekuat sekarang. Siapa tau cerita kamu bisa bikin orang lain sehebat kamu juga loh, <br> yuk saling sharing!</p>
                     <p class="text-center font-size-16 font-weight-400 mt-3 d-block d-md-none">“kamu terlalu berharga untuk kalimat <b>“...aku gk berguna...”</b>. Kamu hebat sudah bisa bertahan sampai detik ini, hal menyakitkan apapun yang pernah kamu alami membuat kamu jadi sekuat sekarang. Siapa tau cerita kamu bisa bikin orang lain sehebat kamu juga loh, yuk saling sharing!</p>
-                    <x-button.secondary-white class="font-size-14 font-weight-400 d-block mx-auto" onclick="location.href = '/project'">Hands my Hand</x-button.secondary-white>
+                    <x-button.secondary-white class="font-size-14 font-weight-400 d-block mx-auto" onclick="location.href = '{{ route('login') }}'">Hands my Hand</x-button.secondary-white>
                 </div>
             </div>
         </div>
@@ -30,6 +30,25 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container margin-top-128">
+            <h1 class="font-size-40 font-weight-600 color-primary-2 text-center mb-5">Cerita Terpopuler</h1>
+            <div class="row" id="pad">
+                @foreach ($data as $item)
+                    <div class="col-md-3 col-6 my-3 d-flex align-items-stretch">
+                        <a href="{{ route('posting', ['id' => $item->id]) }}">
+                            <x-card.rounded see="{{ $item->see }}" like="{{ $item->like }}"
+                                title="{{ $item->title }}" btn="Baca"
+                                img="{{ $item->cover ? asset('images/' . $item->cover) : asset('images/lost_home.jpg') }}">
+                            </x-card.rounded>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+            <x-button.secondary-white class="font-size-14 font-weight-400 d-block mx-auto mt-4" onclick="location.href = '{{ route('beranda') }}'">See More</x-button.secondary-white>
         </div>
     </section>
 
@@ -67,4 +86,47 @@
             </div>
         </div>
     </section>
+
+    @push('custom-script')
+        {{-- <script>
+            let visibilitas = 1;
+            $('#visibilitas').on('change', function() {
+                visibilitas = $(this).val();
+                fetchData();
+            })
+
+            function fetchData() {
+                $.ajax({
+                    url: `fetchData/${visibilitas}`,
+                    method: "GET",
+                    headers:{
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(data) {
+                        // console.log(data.data);
+                        let element = "";
+                        $.each(data.data, function(index, value) {
+                            element += `
+                                <div class="col-md-3 col-6 my-3 d-flex align-items-stretch">
+                                    <a href="edit/${value.id}">
+                                        <x-card.rounded see="${value.see}" like="${value.like}" title="${value.title}" btn="Sunting" img="{{ URL::asset('images/${value.cover}') }}">
+                                            <a href="delete/${value.id}">
+                                                <x-button.primary-white class="w-100 mt-3">Hapus</x-button.primary-white>
+                                            </a>
+                                        </x-card.rounded>
+                                    </a>
+                                </div>
+                            `;
+                        })
+
+                        $("#pad").html(element)
+                        // console.log(data)
+                    },
+                    error: function(error) {
+                        console.log(error.message);
+                    }
+                })
+            }
+        </script> --}}
+    @endpush
 @endsection
